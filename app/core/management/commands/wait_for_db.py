cb7d188a7a1c1483fc62ecb80a80f07146493230
fauzan <fauzan@dawrat.com>
2023-12-13 13:18:09 +0530

    Created custom user model

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -8,9 +8,10 @@ from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """Django command to wait for database."""
-    
+
     def handle(self, *args, **options):
         """Entrypoint for command"""
         self.stdout.write('Waiting for database...')
@@ -22,6 +23,5 @@ class Command(BaseCommand):
             except (Psycopg2Error, OperationalError):
                 self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
-                
-        self.stdout.write(self.style.SUCCESS('Database available!'))    
-        
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
